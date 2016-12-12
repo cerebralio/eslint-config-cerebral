034c56957bb1839b82e2fc585a3de2412706d54f
Alex Scheel Meyer <alexscheelmeyer@gmail.com>
2016-12-12 20:41:38 +0100

    removed restriction for class methods

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = {
   rules: {
     "arrow-parens": ["error", "always"], // always want parentheses for arrow functions
     "no-param-reassign": 0,              // allows to reassign parameters (often helpful)
-    "no-console": 0,                     // console logging is very helpful for debugging
+    "no-console": 0,                     // many logging tools expect stdout / stderr for logs
+    "class-methods-use-this": 0,         // just because you can make it static does not mean that you should
   }
 };
