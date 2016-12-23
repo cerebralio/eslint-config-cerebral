c198f676e2f546cae12db0954ab75ee33e6feb81
Alex Scheel Meyer <alexscheelmeyer@gmail.com>
2016-12-23 10:59:39 +0100

    further react setup

--- react.js
+++ react.js
@@ -1,8 +1,23 @@
 module.exports = {
+  "plugins": [
+    "react",
+  ],
+  "env": {
+    "es6": true,
+  },
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true,
+      "spread": true,
+    }
+  },
   extends: [
     require.resolve('./index'),
   ],
   rules: {
     "arrow-parens": ["error", "as-needed"],  // for react chained arrows are often used, better without parens
+    "react/jsx-uses-vars": "error",          // this is to avoid errors for vars only used in JSX
   }
 };
