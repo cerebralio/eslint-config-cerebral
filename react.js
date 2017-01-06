6087d31f86d44ae87b22cae4bfb2ba6d0178a46a
Alex Scheel Meyer <alexscheelmeyer@gmail.com>
2017-01-06 19:45:16 +0100

    added no-bind rule

--- react.js
+++ react.js
@@ -20,5 +20,6 @@ module.exports = {
   rules: {
     "arrow-parens": ["error", "as-needed"],  // for react chained arrows are often used, better without parens
     "react/jsx-uses-vars": "error",          // this is to avoid errors for vars only used in JSX
+    "react/jsx-no-bind": "error",            // dont want endless function creation
   }
 };
