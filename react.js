5fef6efb4ea935cb7c866ca0c3393fae9f1dd66b
Alex Scheel Meyer <alexscheelmeyer@gmail.com>
2017-01-09 20:59:53 +0100

    added rule for allowing pure jsx and importing react

--- react.js
+++ react.js
@@ -19,6 +19,7 @@ module.exports = {
   ],
   rules: {
     "arrow-parens": ["error", "as-needed"],  // for react chained arrows are often used, better without parens
+    "react/jsx-uses-react": "error" ,        // do not consider it an error to include React when only using JSX
     "react/jsx-uses-vars": "error",          // this is to avoid errors for vars only used in JSX
     "react/jsx-no-bind": "error",            // dont want endless function creation
   }
